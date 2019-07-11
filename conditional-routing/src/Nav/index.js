0e2d3ea52b276d7a5afb87b2eeee6bba52f32262
Cameron Ray <27529775+upsmancsr@users.noreply.github.com>
2019-07-11 15:27:15 -0700

    Nav functional compoent added to Nav/index.js.

---
+++ conditional-routing/src/Nav/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Nav.css';
+import logo from './cameron-crest';
+
+const Nav = () => (
+    <nav>
+    <img src={logo} alt="" />
+    </nav>
+);
+export default Nav;
