09189b50d26d48a9281c0f367d35e0065366f99e
Cameron Ray <27529775+upsmancsr@users.noreply.github.com>
2019-07-11 16:58:57 -0700

    UsersList added.

--- conditional-routing/src/Users/UsersList.js
+++ conditional-routing/src/Users/UsersList.js
@@ -10,3 +10,23 @@ const listOfUsersPerRow = (users, row, itemsPerRow, match) => {
         });
 }
 
+const listOfRows = (users, itemsPerRow, match) => {
+    const numberOfUsers = users.length;
+    const rows = Math.ceil(numberOfUsers / itemsPerRow);
+    return Array(rows)
+        .fill()
+        .map((val, rowIndex) => (
+            <div className="columns">
+                {listOfUsersPerRow(users, rowIndex + 1, itemsPerRow, match)}
+            </div>
+        ));
+};
+
+const UsersList = ({ users, itemsPerRow = 2, match }) => (
+<div className="cards">
+    <h3 className="is-size-3 has-text-centered">Users</h3>
+    {listOfRows(users, itemsPerRow, match)}
+</div>
+);
+
+export default UsersList;
